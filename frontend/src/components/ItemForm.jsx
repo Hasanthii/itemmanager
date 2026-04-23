b68d4141c226c3c4cac0812699af2a2030d3e598
Hasanthii <hasanthichamoda3@gmail.com>
2026-04-24 00:29:27 +0530

    created frontend

---
+++ frontend/src/components/ItemForm.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { createItem } from "../api";
+
+export default function ItemForm({ onItemAdded }) {
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [price, setPrice] = useState(""); 
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await createItem({ name, description, price: parseFloat(price) });
+        onItemAdded();
+        setName("");
+        setDescription("");
+        setPrice("");
+    };
+
+    return (    
+        <form onSubmit={handleSubmit} style={{marginBottom:'2rem'}}>
+            <h2>Add new Item</h2>
+            <div>
+                <input
+                    placeholder="Item name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required   
+                />
+            </div>
+            <div>
+                <input
+                    placeholder="Description"       
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    required
+                />
+            </div>
+            <div>
+                <input
+                    placeholder="Price"
+                    type="number"   
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                    required
+                    
+                />
+            </div>
+            <button type="submit">Add Item</button>
+        </form>
+    );
+}
+
+            
+            
